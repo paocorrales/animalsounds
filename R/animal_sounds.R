9e77f8ec135982b355504960530232f61f26ec13
EllaKaye <ella.kaye@gmail.com>
2025-06-16 10:44:21 +0100

    Add documentation for animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,13 @@
+#' Print the Sound than an Animal Makes
+#'
+#' @param animal A character string
+#' @param sound A character string
+#'
+#' @export
+#' @returns A string saying what sound the animal makes
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
